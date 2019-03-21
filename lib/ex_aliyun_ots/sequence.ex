28ff85f7c0b1832428d192c9877d4c63564684dc
xinz <joy2everyone.z@gmail.com>
2019-03-21 14:34:08 +0800

    Update sequence use atomic increment

--- lib/ex_aliyun_ots/sequence.ex
+++ lib/ex_aliyun_ots/sequence.ex
@@ -1,16 +1,11 @@
 defmodule ExAliyunOts.Sequence do
   require Logger
 
-  alias ExAliyunOts.{Var, Client}
-  alias ExAliyunOts.Const.{OperationType, ReturnType, RowExistence, PKType, FilterType, ComparatorType}
-
-  require OperationType
+  alias ExAliyunOts.Const.{ReturnType, PKType}
   require ReturnType
-  require RowExistence
   require PKType
-  require FilterType
-  require ComparatorType
 
+  use ExAliyunOts.Mixin
   use Retry
 
   @primary_key_name "name"
@@ -18,19 +13,16 @@ defmodule ExAliyunOts.Sequence do
   @retry_delay_max 3_000 # 3 second
 
   def create(instance_name, sequence) do
-    var_to_create = %Var.CreateTable{
-      table_name: sequence.name,
-      primary_keys: [{@primary_key_name, PKType.string}],
+    create_table instance_name, sequence.name,
+      [{@primary_key_name, PKType.string}],
       reserved_throughput_write: sequence.reserved_throughput_write,
       reserved_throughput_read: sequence.reserved_throughput_read,
       deviation_cell_version_in_sec: sequence.deviation_cell_version_in_sec
-    }
-    Client.create_table(instance_name, var_to_create)
   end
 
   def next_value(instance_name, var_get_seq_next) do
     result = retry_while with: exponential_backoff() |> randomize() |> cap(@retry_delay_max) do
-      case remote_next_value(instance_name, var_get_seq_next) do
+      case generate_next_value(instance_name, var_get_seq_next) do
         res = {:error, _error} ->
           {:cont, res}
         res ->
@@ -41,89 +33,34 @@ defmodule ExAliyunOts.Sequence do
     result
   end
 
-  defp remote_next_value(instance_name, var_get_seq_next) do
-    var_get_row = %Var.GetRow{
-      table_name: var_get_seq_next.name,
-      primary_keys: [{@primary_key_name, var_get_seq_next.event}],
-      columns_to_get: [@value_column]
-    }
-    result = Client.get_row(instance_name, var_get_row)
-    case result do
-      {:ok, get_row_response} ->
-        row = get_row_response.row
-        if row == nil do
-          init_value = var_get_seq_next.starter
-          new_value = init_value + var_get_seq_next.increment_offset
-          filter = %Var.Filter{
-            filter_type: FilterType.single_column,
-            filter: %Var.SingleColumnValueFilter{
-              comparator: ComparatorType.eq,
-              column_name: @value_column,
-              column_value: new_value,
-              ignore_if_missing: true
-            }
-          }
-          condition = %Var.Condition{
-            row_existence: RowExistence.expect_not_exist,
-            column_condition: filter
-          }
-          update(instance_name, var_get_seq_next, condition, new_value)
-        else
-          {_pk, [{@value_column, current_value, _last_update_timestamp}]} = row
-          new_value = current_value + var_get_seq_next.increment_offset
-          filter = %Var.Filter{
-            filter_type: FilterType.single_column,
-            filter: %Var.SingleColumnValueFilter{
-              comparator: ComparatorType.eq,
-              column_name: @value_column,
-              column_value: current_value,
-              ignore_if_missing: false
-            }
-          }
-          condition = %Var.Condition{
-            row_existence: RowExistence.expect_exist,
-            column_condition: filter
-          }
-          update(instance_name, var_get_seq_next, condition, new_value)
-        end
-      {:error, error} ->
-        Logger.error("** ExAliyunOts: generate the next value when query occur error: #{inspect error}, will retry it")
-        {:error, error}
-    end
-  end
-
   def delete(instance_name, sequence_name) do
-    Client.delete_table(instance_name, sequence_name)
+    delete_table(instance_name, sequence_name)
   end
 
   def delete_event(instance_name, sequence_name, event) do
-    var_to_delete = %Var.DeleteRow{
-      table_name: sequence_name,
-      primary_keys: [{@primary_key_name, event}],
-      condition: %Var.Condition{
-        row_existence: RowExistence.expect_exist
-      }
-    }
-    Client.delete_row(instance_name, var_to_delete)
+    delete_row instance_name, sequence_name,
+      [{@primary_key_name, event}],
+      condition: condition(:expect_exist)
   end
 
-  defp update(instance_name, var_get_seq_next, condition, new_value) do
-    var_update_row = %Var.UpdateRow{
-      table_name: var_get_seq_next.name,
-      primary_keys: [{@primary_key_name, var_get_seq_next.event}],
-      updates: %{
-        OperationType.put => [{@value_column, new_value}]
-      },
-      condition: condition
-    }
-    result = Client.update_row(instance_name, var_update_row)
-    case result do
-      {:ok, _update_response} ->
-        new_value
-      {:error, error} ->
-        Logger.error("** ExAliyunOts: generate the next value when update occur error: #{inspect error}, will retry it")
-        {:error, error}
-    end
+  defp generate_next_value(instance_name, var) do
+    result =
+      update_row instance_name, var.name,
+        [{@primary_key_name, var.event}],
+        increment: [{@value_column, var.increment_offset}],
+        return_type: ReturnType.after_modify,
+        return_columns: [@value_column],
+        condition: condition(:ignore)
+    return_value(result)
+  end
+
+  defp return_value({:ok, response}) do
+    {_pk, [{@value_column, value, _}]} = response.row
+    value
+  end
+  defp return_value({:error, error}) do
+    Logger.error("** ExAliyunOts: generate the next value when update occur error: #{inspect error}, will retry it")
+    {:error, error}
   end
 
 end
--- test/sequence_test.exs
+++ test/sequence_test.exs
@@ -2,34 +2,29 @@ defmodule ExAliyunOtsTest.Sequence do
   use ExUnit.Case
   require Logger
 
-  @instance_name "super-test"
+  @instance_name "edc-ex-test"
   alias ExAliyunOts.Var
   alias ExAliyunOts.Sequence
 
   test "next value" do
-    cur_timestamp = Timex.to_unix(Timex.now())
-    sequence_name = "test_seq_#{cur_timestamp}"
-    var_new_seq = %Var.NewSequence{
+    sequence_name = "test_sequence"
+    var_new = %Var.NewSequence{
       name: sequence_name
     }
-    result = Sequence.create(@instance_name, var_new_seq)
+    result = Sequence.create(@instance_name, var_new)
     assert result == :ok
     Process.sleep(3_000)
 
-    # concurrency test
     concurrency_size = 10
     stream = Task.async_stream(1..concurrency_size, fn(_index) -> 
-      var_next_val = %Var.GetSequenceNextValue{
+      var_next = %Var.GetSequenceNextValue{
         name: sequence_name,
       }
-      Sequence.next_value(@instance_name, var_next_val)
+      Sequence.next_value(@instance_name, var_next)
     end, timeout: :infinity, max_concurrency: concurrency_size)
 
-    result = Enum.map(stream, fn({:ok, item}) -> item end)
-    assert length(result) == concurrency_size
-
-    distinct_length = MapSet.new(result) |> MapSet.to_list |> length
-    assert distinct_length == concurrency_size
+    result = Enum.map(stream, fn({:ok, item}) -> item end) |> MapSet.new()
+    assert MapSet.size(result) == concurrency_size
     assert Enum.sort(result) == Enum.map(1..concurrency_size, fn(item) -> item end)
 
     del_result = Sequence.delete_event(@instance_name, sequence_name, "default")
