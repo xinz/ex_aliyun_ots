14c14d0c04568d1f63baba778a177b4088c5d6f7
xinz <joy2everyone.z@gmail.com>
2019-07-23 18:57:06 +0800

    Version 0.3.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExAliyunOts.Mixfile do
   def project do
     [
       app: :ex_aliyun_ots,
-      version: "0.3.2",
+      version: "0.3.3",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env == :prod,
