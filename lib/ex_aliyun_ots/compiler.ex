1e6b3f78123087ba45f6dac809906c94c088807b
xinz <joy2everyone.z@gmail.com>
2020-03-02 22:46:04 +0800

    Breakdown pre-defined functions in compiler

--- lib/ex_aliyun_ots/compiler.ex
+++ lib/ex_aliyun_ots/compiler.ex
@@ -10,13 +10,18 @@ defmodule ExAliyunOts.Compiler do
     quote do
       @instance unquote(instance)
 
-      unquote(basic_functions())
+      unquote(table_functions())
+      unquote(row_functions())
+      unquote(row_batch_helper_functions())
+      unquote(local_transaction_functions())
+      unquote(search_functions())
       unquote(search_helper_functions())
     end
   end
 
-  defp basic_functions() do
+  defp table_functions() do
     quote do
+
       def create_table(table, pk_keys, opts \\ []) do
         ExAliyunOts.create_table(@instance, table, pk_keys, opts)
       end
@@ -36,6 +41,12 @@ defmodule ExAliyunOts.Compiler do
       def describe_table(table) do
         ExAliyunOts.describe_table(@instance, table)
       end
+
+    end
+  end
+
+  defp row_functions() do
+    quote do
 
       def batch_get(requests) do
         ExAliyunOts.batch_get(@instance, requests)
@@ -61,22 +72,6 @@ defmodule ExAliyunOts.Compiler do
         ExAliyunOts.delete_row(@instance, table, pk_keys, opts)
       end
 
-      def get(table, pk_keys, opts \\ []) do
-        ExAliyunOts.get(table, pk_keys, opts)
-      end
-
-      def write_put(pk_keys, attrs, opts \\ []) do
-        ExAliyunOts.write_put(pk_keys, attrs, opts)
-      end
-
-      def write_update(pk_keys, opts \\ []) do
-        ExAliyunOts.write_update(pk_keys, opts)
-      end
-
-      def write_delete(pk_keys, opts \\ []) do
-        ExAliyunOts.write_delete(pk_keys, opts)
-      end
-
       def get_range(table, inclusive_start_primary_keys, exclusive_end_primary_keys, opts \\ []) do
         ExAliyunOts.get_range(@instance, table, inclusive_start_primary_keys, exclusive_end_primary_keys, opts)
       end
@@ -84,6 +79,12 @@ defmodule ExAliyunOts.Compiler do
       def iterate_all_range(table, inclusive_start_primary_keys, exclusive_end_primary_keys, opts \\ []) do
         ExAliyunOts.iterate_all_range(@instance, table, inclusive_start_primary_keys, exclusive_end_primary_keys, opts)
       end
+
+    end
+  end
+
+  defp search_functions() do
+    quote do
 
       def search(table, index_name, opts \\ []) do
         ExAliyunOts.search(@instance, table, index_name, opts)
@@ -100,6 +101,34 @@ defmodule ExAliyunOts.Compiler do
       def describe_search_index(table, index_name) do
         ExAliyunOts.describe_search_index(@instance, table, index_name)
       end
+
+    end
+  end
+
+  defp row_batch_helper_functions() do
+    quote do
+
+      def get(table, pk_keys, opts \\ []) do
+        ExAliyunOts.get(table, pk_keys, opts)
+      end
+
+      def write_put(pk_keys, attrs, opts \\ []) do
+        ExAliyunOts.write_put(pk_keys, attrs, opts)
+      end
+
+      def write_update(pk_keys, opts \\ []) do
+        ExAliyunOts.write_update(pk_keys, opts)
+      end
+
+      def write_delete(pk_keys, opts \\ []) do
+        ExAliyunOts.write_delete(pk_keys, opts)
+      end
+
+    end
+  end
+
+  defp local_transaction_functions() do
+    quote do
 
       def start_local_transaction(table, partition_key) do
         ExAliyunOts.start_local_transaction(@instance, table, partition_key)
