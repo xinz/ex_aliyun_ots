f36e87520cc05f78d962b6db0709960788fe7be0
xinz <joy2everyone.z@gmail.com>
2020-05-27 12:52:40 +0800

    Add global secondary index test

---
+++ test/mixin/secondary_index_test.exs
@@ -0,0 +1,23 @@
+defmodule ExAliyunOts.MixinTest.SecondaryIndexTest do
+  use ExAliyunOts,
+    instance: EDCEXTestInstance
+
+  use ExUnit.Case
+
+  @index_name "TestIndex"
+
+  test "global secondary index test" do
+
+    {:ok, item} = get_row(@index_name, [{"name", "namea"}, {"key", "1"}])
+    assert item != nil
+
+    inclusive = [{"name", "namea"}, {"key", :inf_min}]
+    exclusive = [{"name", "namea"}, {"key", :inf_max}]
+    {:ok, response} = get_range(@index_name, inclusive, exclusive)
+
+    [{pks, _attr}] = response.rows
+    [{"name", "namea"}, {"key", key}] = pks
+    assert key == "1"
+  end
+
+end
