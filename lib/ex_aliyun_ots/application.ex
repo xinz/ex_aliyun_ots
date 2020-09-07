d50ba91118b5fa183f959eb5ff4572ffbc465ee4
xinz <joy2everyone.z@gmail.com>
2020-09-07 12:24:33 +0800

    Remove Mix in runtime

--- lib/ex_aliyun_ots/application.ex
+++ lib/ex_aliyun_ots/application.ex
@@ -7,42 +7,45 @@ defmodule ExAliyunOts.Application do
 
   import Supervisor.Spec, warn: false
 
-  @app Mix.Project.config()[:app]
-
   def start(_type, _args) do
+    app = Application.get_application(__MODULE__)
+    enable_tunnel = Application.get_env(app, :enable_tunnel, false)
+    instances = Application.get_env(app, :instances, [])
+
     children =
       child_spec(
-        Application.get_env(@app, :enable_tunnel, false),
-        Application.get_env(@app, :instances, [])
+        app,
+        enable_tunnel,
+        instances
       )
 
     Supervisor.start_link(children, strategy: :one_for_one)
   end
 
-  defp child_spec(_enable_tunnel, []) do
+  defp child_spec(_app, _enable_tunnel, []) do
     raise ExAliyunOts.RuntimeError, "Not found any instances configuration"
   end
 
-  defp child_spec(true, instance_keys) do
+  defp child_spec(app, true, instance_keys) do
     [
-      worker(ExAliyunOts.Config, [load_instances(instance_keys)]),
+      worker(ExAliyunOts.Config, [load_instances(app, instance_keys)]),
       worker(ExAliyunOts.Tunnel.Registry, []),
       supervisor(ExAliyunOts.Tunnel.DynamicSupervisor, [])
     ]
   end
 
-  defp child_spec(false, instance_keys) do
+  defp child_spec(app, false, instance_keys) do
     [
-      worker(ExAliyunOts.Config, [load_instances(instance_keys)])
+      worker(ExAliyunOts.Config, [load_instances(app, instance_keys)])
     ]
   end
 
-  defp load_instances(instance_keys) do
+  defp load_instances(app, instance_keys) do
     Enum.reduce(instance_keys, [], fn instance_key, acc ->
       [
         {
           instance_key,
-          Map.merge(%Instance{}, Enum.into(Application.get_env(@app, instance_key), %{}))
+          Map.merge(%Instance{}, Enum.into(Application.get_env(app, instance_key), %{}))
         }
         | acc
       ]
