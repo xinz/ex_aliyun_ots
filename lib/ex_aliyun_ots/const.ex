ca5d01fc19beb369f41856ad7b1176c7f59fb8df
Kevin Pan <feng_19@foxmail.com>
2021-01-12 11:52:33 +0800

    feat: Do not log error for OTSObjectAlreadyExist & OTSObjectNotExist. (#25)
    * feat: Do not log error for OTSObjectAlreadyExist.
    
    * feat: Do not log error for OTSObjectNotExist.

--- lib/ex_aliyun_ots/const.ex
+++ lib/ex_aliyun_ots/const.ex
@@ -76,6 +76,9 @@ defmodule ExAliyunOts.Const.ErrorType do
   const(:storage_timeout, "OTSTimeout")
   const(:server_unavailable, "OTSServerUnavailable")
   const(:internal_server_error, "OTSInternalServerError")
+  const(:condition_check_fail, "OTSConditionCheckFail")
+  const(:object_already_exist, "OTSObjectAlreadyExist")
+  const(:object_not_exist, "OTSObjectNotExist")
 
   const(
     :too_frequent_reserved_throughput_adjustment,
